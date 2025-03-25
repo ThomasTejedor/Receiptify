329b308e31f580235b7378127ebb80462833316c
Thomas Tejedor <tctejedor@yahoo.com>
2025-03-25 00:34:02 -0700

    Firebase working, setting up login-page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import '../pages/startup_screen.dart';
+import '../pages/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -18,10 +20,12 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Receiptify',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 13, 19, 33),
         useMaterial3: true,
       ),
-      home: StartupPage(),
+      home: Scaffold(
+        body: LoginPage()
+      ),
     );
   }
 }

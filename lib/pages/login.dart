1432bb12c56744ba8ba4abbf62b6f1847bdf28f9
Thomas Tejedor <tctejedor@yahoo.com>
2025-03-25 02:00:54 -0700

    Added most of the UI for log-in page, still need routing

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 
 class LoginPage extends StatefulWidget {
@@ -18,13 +19,19 @@ class _LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
   }
 
   Widget _buildUI() {
+    Map userData = {};
+    final _formkey = GlobalKey<FormState>();
+
     return SafeArea(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           
+          //Logo or Logo Name
           Padding(
-            padding: const EdgeInsets.only(top: 10),
+            padding: const EdgeInsets.only(
+              top: 10,
+            ),
             child: Center(
               child: const Text(
                 'Receiptify',
@@ -36,31 +43,125 @@ class _LoginPageState extends State<LoginPage> with WidgetsBindingObserver {
               ),
             ),
           ),
-          Column (
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                top: 100,
-                left: 50,
-                right: 50),
+          //Area for user input and login button
+          Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 50
+            ),
+            child: Form(
+              key: _formkey,
+              child: Column(
                 
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: TextButton(
-                    style: TextButton.styleFrom (
-                      backgroundColor: const Color.fromARGB(255, 62, 92, 118),
-                      foregroundColor: const Color.fromARGB(255, 13, 19, 33),
-                      
-                    ),
-                    onPressed: () async {},
-                    child: const Text('Login')
+                children: [
+                  //Create the email user text input
+                  Padding( 
+                    padding: const EdgeInsets.all(12.0), 
+                    child: TextFormField( 
+                      validator: MultiValidator([ 
+                        RequiredValidator( 
+                            errorText: 'Enter email address'), 
+                        EmailValidator( 
+                            errorText: 
+                                'Please correct email field'), 
+                      ]).call, 
+                      decoration: InputDecoration( 
+                        hintText: 'Email', 
+                        labelText: 'Email', 
+                        prefixIcon: Icon( 
+                          Icons.email, 
+                          //color: Colors.green, 
+                        ), 
+                        errorStyle: TextStyle(fontSize: 18.0), 
+                        border: OutlineInputBorder( 
+                          borderSide: 
+                            BorderSide(color: Colors.red), 
+                          borderRadius: BorderRadius.all( 
+                            Radius.circular(9.0)
+                          )   
+                        )
+                      )
+                    )
                   ),
-                ),
+                  //Create the password user input field
+                  Padding( 
+                    padding: const EdgeInsets.all(12.0), 
+                    child: TextFormField( 
+                      validator: MultiValidator([ 
+                        RequiredValidator( 
+                          errorText: 'Please enter Password'), 
+                        MinLengthValidator(8, 
+                          errorText: 
+                            'Password must be atlist 8 digit'), 
+                        PatternValidator(r'(?=.*?[#!@$%^&*-])', 
+                          errorText: 
+                            'Password must be atlist one special character') 
+                      ]).call, 
+                      decoration: InputDecoration( 
+                        hintText: 'Password', 
+                        labelText: 'Password', 
+                        prefixIcon: Icon( 
+                          Icons.key, 
+                        ), 
+                        errorStyle: TextStyle(fontSize: 18.0), 
+                        border: OutlineInputBorder( 
+                          borderSide: BorderSide(color: Colors.red), 
+                          borderRadius: 
+                            BorderRadius.all(
+                              Radius.circular(9.0)
+                            )
+                        ), 
+                      ), 
+                    ), 
+                  ), 
+                  //Create the Login Button
+                  Padding(
+                    padding: const EdgeInsets.only(top: 12, bottom: 5),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: TextButton(
+                        style: TextButton.styleFrom (
+                          backgroundColor: const Color.fromARGB(255, 62, 92, 118),
+                          foregroundColor: const Color.fromARGB(255, 13, 19, 33),
+                          
+                        ),
+                        onPressed: () {
+                          if (_formkey.currentState!.validate()) {
+                            print('form submitted');
+                          }
+                        },
+                        child: const Text('Login')
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: RichText(
+                      textAlign: TextAlign.left,
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Dont have an account?',
+                            style: new TextStyle(color: const Color.fromARGB(255, 62, 92, 118)),
+                          ),
+                          TextSpan(
+                            text: '  Sign up',
+                            style: new TextStyle(color: Colors.blue),
+                            recognizer: new TapGestureRecognizer()
+                              ..onTap = () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ]
               ),
-            ]
+            ),
           ),
-        ],
+        ]
       ),
     );
+
   }
+  
 }

995f00f7f5e7404106bd34f396c8b980904a2a9f
Thomas Tejedor <tctejedor@yahoo.com>
2025-04-28 21:27:27 -0700

    trying to solve final publish problems

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.example.receiptify"
+    namespace = "com.receiptify.receiptify"
     compileSdk = 35
     ndkVersion = "27.0.12077973"
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.receiptify"
+        applicationId = "com.receiptify.receiptify"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
@@ -65,5 +65,8 @@ flutter {
 dependencies {
 
     implementation("com.google.android.material:material:1.12.0")
-
+    implementation("com.google.mlkit:text-recognition-chinese:16.0.0")
+    implementation("com.google.mlkit:text-recognition-devanagari:16.0.0")
+    implementation("com.google.mlkit:text-recognition-japanese:16.0.0")
+    implementation("com.google.mlkit:text-recognition-korean:16.0.0")
 }
